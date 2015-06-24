6d6e68588986038bb8d5d0ac97a3f208c90b82af
lorak <k.zysk@zoho.com>
2015-06-24 19:19:57 +0200

    implement google maps error message

--- js/map.js
+++ js/map.js
@@ -7,4 +7,26 @@ function initialize() {
         mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function loadScript() {
+  var API_KEY = "AIzaSyDfBRThn5FwVjH451Lo-oeK-rGHsPRVzWg",
+      script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&sensor=TRUE_OR_FALSE&callback=initialize";
+
+  setTimeout(function () {
+        try{
+            if (!google || !google.maps) {
+                //This will Throw the error if 'google' is not defined
+            }
+        }
+        catch (e) {
+        	var $error_elem = $('#map-canvas');
+           //alert('google not reachable');
+            $error_elem.text('Sorry, An Error Occured. Google Maps Could Not Be Reached.');
+        }
+    }, 5000);
+  
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
