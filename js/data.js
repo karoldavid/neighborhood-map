6e12b004b2e2c6eafb459dde723f44576d00c00b
lorak <k.zysk@zoho.com>
2015-06-26 16:27:53 +0200

    move all data to js/data.js

---
+++ js/data.js
@@ -0,0 +1,41 @@
+ var neighborhood = {
+		center: {
+			"name": "Warsaw",
+			"coord": {lat: 52.232938, lng:  21.0611941}
+	    }
+	 };
+
+
+ var restaurants = [
+        {
+            name: "Bar mleczny",
+            address: "Marymoncka 49, Warszawa - Poland",
+            coord: {lat : 52.28091000000001, lng: 20.958057000000053},
+            options: "Lunch"
+        },
+        {
+            name: "Restauracja Polska Rozana",
+            address: "Chocimska 7, Warsaw - Poland",
+            coord: {lat: 52.2083889, lng: 21.023467900000014},
+            options: "Lunch, Dinner, Dessert"
+        },
+        {
+            name: "Stara Kamienica",
+            address: "Str. Widok 8, Warsaw - Poland",
+            coord: {lat: 52.2315629, lng: 21.0151429},
+            options: "Brunch, Lunch, Dinner, Dessert"
+        },
+        {
+            name: "U Kucharzy",
+            address: "Długa 52, Warsaw - Poland",
+            coord: {lat: 52.2456709, lng: 21.001365899999996},
+            options: "Dinner, Dessert"
+        }];
+
+
+var locations = [
+        ["Bar mleczny", "Marymoncka 49, Warszawa - Poland"],
+        ["Restauracja Polska Rozana", "Chocimska 7, Warsaw - Poland"],
+        ["Stara Kamienica", "Str. Widok 8, Warsaw - Poland"],
+        ["U Kucharzy","Długa 52, Warsaw - Poland"]
+    ];
--- js/map.js
+++ js/map.js
@@ -1,25 +1,51 @@
-var neighborhood = {
-		location: {
-			"name": "Warsaw",
-			"coord": [{"lat": 52.232938, "lng":  21.0611941}]
-		}
-	};
-    
+
+   
 function initialize() {
-	var latitude = neighborhood.location["coord"][0].lat,
-	    longitude = neighborhood.location["coord"][0].lng;
+	var latitude = neighborhood.center["coord"].lat,
+	    longitude = neighborhood.center["coord"].lng;
 
 	var mapOptions = {
-        center: { lat: latitude , lng: longitude},
+        center: {lat: latitude , lng: longitude},
         zoom: 12
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+    // var infowindow = new google.maps.InfoWindow();
+    // var geocoder = new google.maps.Geocoder();
+
+    // var marker, i;
+
+    // for (i = 0; i < locations.length; i++) {
+
+    //     geocoder.geocode( { 'address': locations[i][1]}, function(results, status) {
+    //         //alert(status);
+    //         if (status == google.maps.GeocoderStatus.OK) {
+
+    //             //alert(results[0].geometry.location);
+    //             console.log(results[0].geometry.location);
+    //             map.setCenter(results[0].geometry.location);
+    //             marker = new google.maps.Marker({
+    //                 position: results[0].geometry.location,
+    //                 map: map
+    //             }); 
+
+    //             google.maps.event.addListener(marker, 'mouseover', function() { infowindow.open(marker, map);});
+    //             google.maps.event.addListener(marker, 'mouseout', function() { infowindow.close();});
+
+    //         }
+    //         else
+    //         {
+    //             alert("some problem in geocode" + status);
+    //         }
+    //     }); 
+    // }
 }
 
 function loadScript() {
   var API_KEY = "AIzaSyDfBRThn5FwVjH451Lo-oeK-rGHsPRVzWg",
       script = document.createElement("script");
+  
   script.type = "text/javascript";
   script.src = "http://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&sensor=TRUE_OR_FALSE&callback=initialize";
 
@@ -40,3 +66,6 @@ function loadScript() {
 }
 
 window.onload = loadScript;
+
+//TODO: Write code required to add map markers identifying a number of locations you are interested in within this neighborhood.
+
