779d8d050aec81f7fc881e31aad41efab6d3cdc0
lorak <k.zysk@zoho.com>
2015-06-27 18:57:18 +0200

    add more sample data

--- js/data.js
+++ js/data.js
@@ -6,36 +6,69 @@
 	 };
 
 
- var restaurants = [
+ var locations = [
         {
             name: "Bar mleczny",
             address: "Marymoncka 49, Warszawa - Poland",
             coord: {lat : 52.28091000000001, lng: 20.958057000000053},
-            options: "Lunch"
+            options: "Lunch",
+            tag: "Restaurant"
         },
         {
             name: "Restauracja Polska Rozana",
             address: "Chocimska 7, Warsaw - Poland",
             coord: {lat: 52.2083889, lng: 21.023467900000014},
-            options: "Lunch, Dinner, Dessert"
+            options: "Lunch, Dinner, Dessert",
+            tag: "Restaurant"
         },
         {
             name: "Stara Kamienica",
             address: "Str. Widok 8, Warsaw - Poland",
             coord: {lat: 52.2315629, lng: 21.0151429},
-            options: "Brunch, Lunch, Dinner, Dessert"
+            options: "Brunch, Lunch, Dinner, Dessert",
+            tag: "Restaurant"
         },
         {
             name: "U Kucharzy",
             address: "Długa 52, Warsaw - Poland",
             coord: {lat: 52.2456709, lng: 21.001365899999996},
-            options: "Dinner, Dessert"
-        }];
+            options: "Dinner, Dessert",
+            tag: "Restaurant"
+        },
+        {   name: "Łazienki Królewskie w Warszawie",
+            address: "Ujazdów, 01-999, Warsaw - Poland",
+            coord: {lat: 52.2152, lng: 21.035},
+            tag:"Park"
+        },
+        {   name: "Lotnisko Chopina",
+            address: "Żwirki i Wigury 1, Warsaw - Poland",
+            coord: {lat: 52.165699, lng: 20.967100},
+            tag: "Airport"
+        },
+        {   name: "Warszawa Centralna",
+            address: "00-844, Warsaw - Poland",
+            coord: {lat: 52.228483, lng: 21.003531},
+            tag: "Train Station"
+        },
+        {
+        	name: "Polonia Palace Hotel",
+        	address: "Aleje Jerozolimskie 45, 00-692, Warsaw - Poland",
+        	coord: {lat: 52.229239, lng: 21.010275},
+        	tag: "Hotel"
 
+        },
+        {
+        	name: "Muzeum Powstania Warszawskiego",
+        	address: "Grzybowska 79, Warsaw - Poland",
+        	coord: {lat: 52.232841, lng: 20.980982},
+        	www: "http://www.1944.pl/"
+        	tag: "Museum"
+        }
+        ];
 
-var locations = [
+/*var locations = [
         ["Bar mleczny", "Marymoncka 49, Warszawa - Poland"],
         ["Restauracja Polska Rozana", "Chocimska 7, Warsaw - Poland"],
         ["Stara Kamienica", "Str. Widok 8, Warsaw - Poland"],
         ["U Kucharzy","Długa 52, Warsaw - Poland"]
-    ];
+    ];*/
