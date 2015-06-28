b03e648aac0dd4a8fcc8508242a54c2dfda1abdd
lorak <k.zysk@zoho.com>
2015-06-28 14:28:17 +0200

    add new locations

--- js/data.js
+++ js/data.js
@@ -1,74 +1,124 @@
+ //@ http://www.latlong.net/
+
  var neighborhood = {
 		center: {
-			"name": "Warsaw",
+			"name": "Warszawa",
 			"coord": {lat: 52.232938, lng:  21.0611941}
 	    }
 	 };
 
 
  var locations = [
+        {   name: "Centrum",
+            address:"Warszawa - Polska",
+            coord: {lat: 52.232938, lng:  21.0611941},
+            tag:"City Center"
+        },
+        {   name: "Stare Miasto",
+            address: "Warszawa - Polska",
+            coord: {lat: 52.247733, lng:  21.013608},
+            tag: "Historic Center"
+        },
+        {   name: "Centrum Kultury Nowy Wspaniały Świat",
+            address: "Świętokrzyska 63, Warszawa - Polska",
+            coord: {lat: 52.233839, lng:  21.001905},
+            www: "http://www.nowywspanialyswiat.pl/",
+            tag: "Cultural Center"
+        },
+        {   name: "Tygmont Jazz Club",
+            address: "Mazowiecka 6/8, Warszawa, Polska",
+            coord: {lat: 52.237082, lng: 21.013234},
+            www: "http://www.tygmont.com.pl/",
+            tag: "Club"
+        },
         {
-            name: "Bar mleczny",
-            address: "Marymoncka 49, Warszawa - Poland",
-            coord: {lat : 52.28091000000001, lng: 20.958057000000053},
+            name: "Familijny Bar Mleczny",
+            address: "Nowy Świat 39, 00-029 Warszawa - Polska",
+            coord: {lat : 52.234173, lng: 21.018673},
             options: "Lunch",
+            video: "http://news.bbc.co.uk/1/hi/programmes/fast_track/9714943.stm",
             tag: "Restaurant"
         },
         {
             name: "Restauracja Polska Rozana",
-            address: "Chocimska 7, Warsaw - Poland",
+            address: "Chocimska 7, Warszawa - Polska",
             coord: {lat: 52.2083889, lng: 21.023467900000014},
             options: "Lunch, Dinner, Dessert",
             tag: "Restaurant"
         },
         {
             name: "Stara Kamienica",
-            address: "Str. Widok 8, Warsaw - Poland",
+            address: "Str. Widok 8, Warszawa - Polska",
             coord: {lat: 52.2315629, lng: 21.0151429},
             options: "Brunch, Lunch, Dinner, Dessert",
             tag: "Restaurant"
         },
         {
             name: "U Kucharzy",
-            address: "Długa 52, Warsaw - Poland",
+            address: "Długa 52, Warszawa - Polska",
             coord: {lat: 52.2456709, lng: 21.001365899999996},
             options: "Dinner, Dessert",
             tag: "Restaurant"
         },
         {   name: "Łazienki Królewskie w Warszawie",
-            address: "Ujazdów, 01-999, Warsaw - Poland",
+            address: "Ujazdów, 01-999, Warszawa - Polska",
             coord: {lat: 52.2152, lng: 21.035},
             tag:"Park"
         },
         {   name: "Lotnisko Chopina",
-            address: "Żwirki i Wigury 1, Warsaw - Poland",
+            address: "Żwirki i Wigury 1, Warszawa - Polska",
             coord: {lat: 52.165699, lng: 20.967100},
             tag: "Airport"
         },
         {   name: "Warszawa Centralna",
-            address: "00-844, Warsaw - Poland",
+            address: "00-844, Warszawa - Polska",
             coord: {lat: 52.228483, lng: 21.003531},
             tag: "Train Station"
         },
         {
         	name: "Polonia Palace Hotel",
-        	address: "Aleje Jerozolimskie 45, 00-692, Warsaw - Poland",
+        	address: "Aleje Jerozolimskie 45, 00-692, Warszawa - Polska",
         	coord: {lat: 52.229239, lng: 21.010275},
         	tag: "Hotel"
 
         },
         {
         	name: "Muzeum Powstania Warszawskiego",
-        	address: "Grzybowska 79, Warsaw - Poland",
+        	address: "Grzybowska 79, Warszawa - Polska",
         	coord: {lat: 52.232841, lng: 20.980982},
         	www: "http://www.1944.pl/",
         	tag: "Museum"
+        },
+        {
+            name: "Plan B",
+            address: "Aleja Wyzwolenia 18, 00-999 Warszawa - Polska",
+            coord: {lat: 52.219978, lng: 21.018735},
+            tag: "Club",
+        },
+        {
+        	name: "Filharmonia Narodowa",
+        	address: "Jasna 5, 00-950 Warszawa - Polska",
+        	coord: {lat: 52.234530, lng: 21.011338},
+        	tag: "Concert Hall"
+        },
+        {
+        	name: "Biblioteka Uniwersytecka",
+        	address: "Dobra 56/66, Warszawa - Polska",
+        	coord: {lat: 52.242392, lng: 21.024425},
+        	tag: "Library"
+
+        },
+        {
+        	name: "Złote Tarasy",
+        	address: "Ulica Złota 59, 00-120 Warszawa",
+        	coord: {lat: 52.230838, lng: 21.002277},
+        	tag: "Shopping Center"
         }
         ];
 
 /*var locations = [
-        ["Bar mleczny", "Marymoncka 49, Warszawa - Poland"],
-        ["Restauracja Polska Rozana", "Chocimska 7, Warsaw - Poland"],
-        ["Stara Kamienica", "Str. Widok 8, Warsaw - Poland"],
-        ["U Kucharzy","Długa 52, Warsaw - Poland"]
+        ["Bar mleczny", "Marymoncka 49, Warszawa - Polska"],
+        ["Restauracja Polska Rozana", "Chocimska 7, Warszawa - Polska"],
+        ["Stara Kamienica", "Str. Widok 8, Warszawa - Polska"],
+        ["U Kucharzy","Długa 52, Warszawa - Polska"]
     ];*/
