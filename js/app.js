e6a566d40ef74fac7995d29f74dec6de88559f82
lorak <k.zysk@zoho.com>
2015-07-14 18:10:13 +0200

    calc distance between map center and Silicon Valley and show as title of citiy string in header

--- js/app.js
+++ js/app.js
@@ -79,9 +79,26 @@ $(document).ready(function(region, focus, locations, styles) {
         this.fs_photos = ko.observableArray([]);
     };
 
+    // @CREDITS: http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1); 
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
     //
     var MyViewModel = function() {
-        console.log(focus['poi'][0]);
+       
         var self = this;
 
         self.query = ko.observable(""); // Search box query string
@@ -90,6 +107,9 @@ $(document).ready(function(region, focus, locations, styles) {
 
         // @TODO: Make map region changeable on click
         self.region = region.center["name"].toUpperCase(); // Map region string for header app info
+        self.distance = Math.round(getDistanceFromLatLonInKm(region.center["coord"].lat, region.center["coord"].lng, 37.362517, -122.03476)) +
+                        ' km to Silicon Valley';
+        console.log(self.distance);
         self.weather = ko.observable(); // Current weather string for header app info
 
         // Sort location list by name property
